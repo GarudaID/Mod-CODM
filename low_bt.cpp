6c441691da0397c735cb0ecf803fde6eceea903b
Kaixin Fukada <48685463+GarudaID@users.noreply.github.com>
2022-11-10 06:59:39 +0700

    ⭐Update low bt version: 1.0.35

--- low_bt.cpp
+++ low_bt.cpp
@@ -1,4 +1,7 @@
-CODM Global 1.0.35 low bt
+//version: 1.0.35
+//mode: Low BT
+//region for: Global version
+                            //region for: Garena next coming up!\\
 
 so=gg.getRangesList('libil2cpp.so')[1].start
 py=0x232E3A4 
