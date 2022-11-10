755c89a7544f050e4dbec516611670759a01558c
Kaixin Fukada <48685463+GarudaID@users.noreply.github.com>
2022-11-10 07:00:39 +0700

    ⭐Update No Reload version: 1.0.35

--- no_reload.cpp
+++ no_reload.cpp
@@ -1,4 +1,7 @@
-CODM Global 1.0.35 no reload
+//version: 1.0.35
+//mode: no reload
+//region for: Global version
+                            //region for: Garena next coming up!\\
 
 so=gg.getRangesList('libil2cpp.so')[1].start
 local py=0x2B8EF90     
