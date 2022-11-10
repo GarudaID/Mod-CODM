58e7e1bc803e41c7cdf00b6ebacfa89109dfcb8b
Kaixin Fukada <48685463+GarudaID@users.noreply.github.com>
2022-11-10 07:04:06 +0700

    ⭐Update Wallhack version: 1.0.35

--- wallhack.cpp
+++ wallhack.cpp
@@ -1,5 +1,9 @@
-CODM Global 1.0.35 wallhack
+//version: 1.0.35
+//mode: wallhack
+//region for: Global version
+                            //region for: Garena next coming up!\\
 
 so=gg.getRangesList('libil2cpp.so')[1].start
 py=0x18DA39C      
 setvalue(so+py,4,0)
+
