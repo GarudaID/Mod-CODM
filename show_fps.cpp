5076e45343730079bbda33ae354194540bb5effe
Kaixin Fukada <48685463+GarudaID@users.noreply.github.com>
2022-11-10 07:02:07 +0700

    ⭐Update Show FPS version: 1.0.35

--- show_fps.cpp
+++ show_fps.cpp
@@ -1,4 +1,7 @@
-CODM Global 1.0.35 Show FPS
+//version: 1.0.35
+//mode: Shop FPS
+//region for: Global version
+                            //region for: Garena next coming up!\\
 
 so=gg.getRangesList("libil2cpp.so")[1].start 
 py=0x2209994 
