91c20dae985410d2b19ee0cc31230449bce6bc69
GarudaID <baharudinkorret@gmail.com>
2022-11-11 20:33:42 +0700

    ⭐Codm CrashFix 1.0.35.0
    ⭐Using Libs [ libanogs.so + libCrashSight.so ]
    ⭐Use On Timi Logo.
    ⭐Working Version {GL,GR,KR,VN}
    ⭐PKG Name Just Garena But Working In All

---
+++ lua/AntiCrash [libanogs.so+libCrashSight.so].lua
@@ -0,0 +1,120 @@
+b=[[32628
+Var #BBBC8658|bbbc8658|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|3658
+Var #BBBC8660|bbbc8660|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|3660
+Var #BBBC90D8|bbbc90d8|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|40d8
+Var #BBBC90DC|bbbc90dc|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|40dc
+Var #BBBC961C|bbbc961c|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|461c
+Var #BBBC9620|bbbc9620|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4620
+Var #BBBC9654|bbbc9654|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4654
+Var #BBBC9658|bbbc9658|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4658
+Var #BBBC965C|bbbc965c|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|465c
+Var #BBBC9660|bbbc9660|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4660
+Var #BBBC9664|bbbc9664|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4664
+Var #BBBC9668|bbbc9668|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4668
+Var #BBBC966C|bbbc966c|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|466c
+Var #BBBC9670|bbbc9670|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4670
+Var #BBBC9674|bbbc9674|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4674
+Var #BBBC9678|bbbc9678|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4678
+Var #BBBC967C|bbbc967c|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|467c
+Var #BBBC9680|bbbc9680|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4680
+Var #BBBC9684|bbbc9684|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4684
+Var #BBBC9688|bbbc9688|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4688
+Var #BBBC968C|bbbc968c|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|468c
+Var #BBBC9690|bbbc9690|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4690
+Var #BBBC9694|bbbc9694|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4694
+Var #BBBC9698|bbbc9698|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4698
+Var #BBBC969C|bbbc969c|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|469c
+Var #BBBC96A0|bbbc96a0|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|46a0
+Var #BBBC96A4|bbbc96a4|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|46a4
+Var #BBBC96A8|bbbc96a8|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|46a8
+Var #BBBC96AC|bbbc96ac|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|46ac
+Var #BBBC9754|bbbc9754|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4754
+Var #BBBC9758|bbbc9758|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4758
+Var #BBBC9764|bbbc9764|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4764
+Var #BBBC9768|bbbc9768|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4768
+Var #BBBC976C|bbbc976c|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|476c
+Var #BBBC9770|bbbc9770|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4770
+Var #BBBC9774|bbbc9774|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4774
+Var #BBBC9780|bbbc9780|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4780
+Var #BBBC9784|bbbc9784|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4784
+Var #BBBC9788|bbbc9788|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4788
+Var #BBBC978C|bbbc978c|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|478c
+Var #BBBC9790|bbbc9790|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4790
+Var #BBBC9794|bbbc9794|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4794
+Var #BBBC9798|bbbc9798|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|4798
+Var #BBBC979C|bbbc979c|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|479c
+Var #BBBC97A0|bbbc97a0|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|47a0
+Var #BBBC97A4|bbbc97a4|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|47a4
+Var #BBBC97A8|bbbc97a8|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|47a8
+Var #BBBC97AC|bbbc97ac|4|0|0|0|0|0|rw-p|/data/app/~~fDXWVKnRNY9hClRIx-8Pxw==/com.garena.game.codm-5EXVX26D7wqBcjzH965u2g==/lib/arm/libCrashSight.so:bss|47ac
+Var #BEDAA708|bedaa708|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35708
+Var #BEDAA70C|bedaa70c|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|3570c
+Var #BEDAA710|bedaa710|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35710
+Var #BEDAA714|bedaa714|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35714
+Var #BEDAA718|bedaa718|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35718
+Var #BEDAA71C|bedaa71c|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|3571c
+Var #BEDAA720|bedaa720|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35720
+Var #BEDAA724|bedaa724|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35724
+Var #BEDAA728|bedaa728|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35728
+Var #BEDAA72C|bedaa72c|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|3572c
+Var #BEDAA730|bedaa730|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35730
+Var #BEDAA734|bedaa734|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35734
+Var #BEDAA738|bedaa738|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35738
+Var #BEDAA73C|bedaa73c|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|3573c
+Var #BEDAA740|bedaa740|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35740
+Var #BEDAA744|bedaa744|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35744
+Var #BEDAA748|bedaa748|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35748
+Var #BEDAA74C|bedaa74c|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|3574c
+Var #BEDAA7DC|bedaa7dc|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|357dc
+Var #BEDAA80C|bedaa80c|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|3580c
+Var #BEDAA814|bedaa814|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35814
+Var #BEDAA818|bedaa818|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35818
+Var #BEDAA81C|bedaa81c|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|3581c
+Var #BEDAA820|bedaa820|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35820
+Var #BEDAA828|bedaa828|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35828
+Var #BEDAA82C|bedaa82c|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|3582c
+Var #BEDAA830|bedaa830|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35830
+Var #BEDAAA8C|bedaaa8c|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35a8c
+Var #BEDAAA90|bedaaa90|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35a90
+Var #BEDAAA94|bedaaa94|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35a94
+Var #BEDAAA98|bedaaa98|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35a98
+Var #BEDAAA9C|bedaaa9c|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35a9c
+Var #BEDAAAA0|bedaaaa0|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35aa0
+Var #BEDAAAA4|bedaaaa4|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35aa4
+Var #BEDAAAA8|bedaaaa8|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35aa8
+Var #BEDAAAAC|bedaaaac|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35aac
+Var #BEDAAAB0|bedaaab0|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35ab0
+Var #BEDAAAB4|bedaaab4|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35ab4
+Var #BEDAAAB8|bedaaab8|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35ab8
+Var #BEDAAAD0|bedaaad0|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35ad0
+Var #BEDAAAD4|bedaaad4|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35ad4
+Var #BEDAAAD8|bedaaad8|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35ad8
+Var #BEDAAADC|bedaaadc|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35adc
+Var #BEDAAAE0|bedaaae0|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35ae0
+Var #BEDAAAE4|bedaaae4|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35ae4
+Var #BEDAAAE8|bedaaae8|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35ae8
+Var #BEDAAAEC|bedaaaec|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35aec
+Var #BEDAAAF0|bedaaaf0|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35af0
+Var #BEDAAAF4|bedaaaf4|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35af4
+Var #BEDAAAF8|bedaaaf8|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35af8
+Var #BEDAAAFC|bedaaafc|4|0|0|0|0|0|rw-p|/data/app/~~ixYi4z4m8DV1RCDgi3Ri_g==/com.garena.game.codm-9eesGJChQ7Pksk28RJSG1Q==/lib/arm/libanogs.so|35afc
+fileData = gg.EXT_STORAGE .. "/[###].dat"
+io.output(fileData):write(b):close()
+gg.loadList(fileData, gg.LOAD_APPEND)
+gg.sleep(50)
+r = gg.getListItems()
+gg.loadResults(r)
+revert = gg.getResults(100000, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(100000, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+if v.flags == gg.TYPE_DWORD then
+v.value = "0"
+v.freeze = true
+end
+end
+gg.addListItems(t)
+t = nil
+
+gg.clearList()
+gg.clearResults()
+
