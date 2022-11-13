48186edc55d08872e11b537b3f91b3ac4353130b
GarudaID <baharudinkorret@gmail.com>
2022-11-13 16:46:29 +0700

    ⭐Update FixCrash(fixBug) 1.0.35
    Run logo✅

--- lua/[New] Logo Bypass.lua
+++ lua/[New] Logo Bypass.lua
@@ -1,13 +1,26 @@
 function prog() end
 function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
 
-
---fix crash
+--killAntiChat
 so = gg.getRangesList('libanogs.so')[1].start 
-py = 0x30a34
+py = 0x30a38
 setvalue(so+py,4,-476053504)
 
 so = gg.getRangesList('libanogs.so')[1].start 
-py = 0x30a38
+py = 0x30a34
 setvalue(so+py,4,-516948194)
-gg.alert("follow github.com/GarudaID ")
+
+--fix crash All Ver
+so = gg.getRangesList('libanogs.so')[1].start 
+py = 0x308c4
+setvalue(so+py,4,0)
+
+so = gg.getRangesList('libanogs.so')[1].start 
+py = 0x308cc
+setvalue(so+py,4,0)
+
+so = gg.getRangesList('libanogs.so')[1].start 
+py = 0x308c8
+setvalue(so+py,4,0)
+
+gg.alert("done")
